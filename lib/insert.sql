7e7be7e5bc27258c7620d23a0fb53556066a9629
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 13:44:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,20 +1,41 @@
-INSERT INTO series (title, author_id, subgenre_id)
-VALUES ("Series1", 1, 2),
-("Series2", 1, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
 
-INSERT INTO subgenres (name)
-VALUES ("Subgenre1"), ("Subgenre2");
+INSERT INTO subgenres (name) VALUES ("Fantasy");
+INSERT INTO subgenres (name) VALUES ("Magic");
 
-INSERT INTO authors (name)
-VALUES ("Author1"), ("Author1");
+INSERT INTO authors (name) VALUES ("J.R.R. Tolkien");
+INSERT INTO authors (name) VALUES ("J.K. Rowling");
 
+INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Two Towers", 1954, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Return of the King", 1954, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Sorcerer's Stone", 1997, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Chamber of Secrets", 1998, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Goblet of Fire", 2000, 2);
 
-INSERT INTO characters (name, motto, species, author_id, series_id)
-VALUES ("Character1", "Motto1", "Specie1", 1, 2),
-("Character1", "Motto1", "Specie1", 2, 1),
-("Character2", "Motto2", "Specie2", 1, 2),
-("Character3", "Motto3", "Specie3", 2, 1),
-("Character4", "Motto4", "Specie4", 1, 2),
-("Character5", "Motto5", "Specie5", 2, 1),
-("Character6", "Motto6", "Specie6", 1, 2),
-("Character7", "Motto7", "Specie7", 2, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "The Boy Who Lived", "Wizard", 2, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Hagrid", "The Gentle Giant", "Giant", 2, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Fluffy", "A bit of music puts em right to sleep", "Three-Headed Dog", 2, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Gilderoy Lockhart", "Me!", "Wizard", 2, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Frodo Baggins", "The Bravest Hobbit", "Hobbit", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Saruman", "The White Wizard", "Wizard", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Lurtz", "An Army Worthy of Mordor", "Uruk-hai", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Shelob", "She likes to feed", "Giant Spider", 1, 1);
+
+INSERT INTO character_books (character_id, book_id) VALUES (1, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (3, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (4, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (7, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (8, 3);
