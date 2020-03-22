9ab692b7d1507e1f0b6c91daa689c9e4a2661850
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 13:35:07 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("La Casa del Papel", 1 , 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Prison Break", 1 , 2);
+INSERT INTO books (title, year, series_id) VALUES ("Book1", 1995, 1);
