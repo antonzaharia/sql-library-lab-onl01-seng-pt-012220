24817ca1c007650e7e8efa27cb40d6abcf51bcef
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 13:44:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,13 +8,6 @@ VALUES ("Subgenre1"), ("Subgenre2");
 INSERT INTO authors (name)
 VALUES ("Author1"), ("Author1");
 
-INSERT INTO books (title, year, series_id)
-VALUES (null, null, null),
-(null, null, null),
-(null, null, null),
-(null, null, null),
-(null, null, null),
-(null, null, null);
 
 INSERT INTO characters (name, motto, species, author_id, series_id)
 VALUES ("Character1", "Motto1", "Specie1", 1, 2),
@@ -25,21 +18,3 @@ VALUES ("Character1", "Motto1", "Specie1", 1, 2),
 ("Character5", "Motto5", "Specie5", 2, 1),
 ("Character6", "Motto6", "Specie6", 1, 2),
 ("Character7", "Motto7", "Specie7", 2, 1);
-
-INSERT INTO character_books (book_id, character_id)
-VALUES (null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null),
-(null, null);
