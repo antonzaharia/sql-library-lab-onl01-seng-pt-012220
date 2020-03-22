3172fbd2d67d237a2ec87cfd48661fd373b922a0
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-22 13:40:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,45 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("La Casa del Papel", 1 , 1);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Prison Break", 1 , 2);
-INSERT INTO books (title, year, series_id) VALUES ("Book1", 1995, 1);
+INSERT INTO series (title, author_id, subgenre_id)
+VALUES ("Series1", 1, 2),
+("Series2", 1, 2);
+
+INSERT INTO subgenres (name)
+VALUES ("Subgenre1"), ("Subgenre2");
+
+INSERT INTO authors (name)
+VALUES ("Author1"), ("Author1");
+
+INSERT INTO books (title, year, series_id)
+VALUES (null, null, null),
+(null, null, null),
+(null, null, null),
+(null, null, null),
+(null, null, null),
+(null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id)
+VALUES ("Character1", "Motto", "Specie", 1, 2),
+(null, null, null, 2, 1),
+(null, null, null, 1, 2),
+(null, null, null, 2, 1),
+(null, null, null, 1, 2),
+(null, null, null, 2, 1),
+(null, null, null, 1, 2),
+(null, null, null, 2, 1);
+
+INSERT INTO character_books (book_id, character_id)
+VALUES (null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null);
